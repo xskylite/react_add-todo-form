35eed3b26c28dca4efc1a97001dc54d98df5adea
xSkyLite <chkalovua11@gmail.com>
2025-05-01 21:36:24 +0300

    fix: getUserById() and getNewTodoId()

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export const App = () => {
   }
 
   function getNewTodoId(): TodoId {
-    return Math.max(...todos.map(todo => todo.id)) + 1;
+    return Math.max(...todos.map(todo => todo.id)) + 1 || 1;
   }
 
   function handleChangeTitle(event: React.FormEvent<HTMLInputElement>) {
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,6 +1,8 @@
 import users from '../api/users';
 import { User } from '../types/User';
 
-export function getUserById(id: number): User | null {
+export type UserId = number;
+
+export function getUserById(id: UserId): User | null {
   return users.find(user => user.id === id) || null;
 }
